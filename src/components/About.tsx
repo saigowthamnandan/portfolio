47a53ac6f4619c51dba9681317eee3edc4d07329
Gowtham Nandan <85236704+saigowthamnandan@users.noreply.github.com>
2025-11-20 21:45:43 +0100

    Update About.tsx
    commented resume connect

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -37,7 +37,7 @@ export default function About() {
             integration.
           </li>
         </ul>
-        <ResumeConnect />
+        {/* <ResumeConnect /> */}
       </div>
     </div>
   );
